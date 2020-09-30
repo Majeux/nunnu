6142b9e16fd1c519963bb0985a48c40cb1380b83
Majeux <manblankestijn@gmail.com>
2020-09-30 22:13:37 +0200

    works but its kind of weird

--- main.cc
+++ main.cc
@@ -2,8 +2,10 @@
 
 int main() {
     Unnuun gen;
+    std::random_device  rand_dev;
+    std::mt19937        generator;
 
-    std::set<uint32_t> nums = gen.rand_n_unique(4, 6);
+    std::set<uint32_t> nums = gen.rand_n_unique(4, 0, 100, [&]() { return generator(); } );
 
     for(auto i : nums)
         std::cerr << i << " ";
--- unnuun.h
+++ unnuun.h
@@ -2,6 +2,7 @@
     Method to generate a random series of Unique Numbers from a Uniform Distribution
 */
 
+#include <functional>
 #include <cassert>
 #include <random>
 #include <set>
@@ -19,4 +20,28 @@ class Unnuun {
         std::set<uint32_t> rand_n_unique(const size_t n);
         std::set<uint32_t> rand_n_unique(const size_t n, size_t max);
         std::set<uint32_t> rand_n_unique(const size_t n, size_t min, size_t max);
+
+        template<typename TF>
+        std::set<uint32_t> rand_n_unique(const size_t n, size_t min, size_t max,
+            TF&& gen_func)
+        {
+            assert(min < max); assert(n < max - min);
+
+            std::set<uint32_t> gen;
+
+            for (size_t i = 0; i < n; i++) {
+                size_t r = min + gen_func()%(max - min);
+
+                for(size_t x : gen) {
+                    if(x <= r) r++;
+                    else break;
+                }
+
+                max--;
+
+                gen.insert(r);
+            }
+
+            return gen;
+        }
 };
