929a4e6862ba97530ca39e4b8271a0a7fb5d6339
Majeux <manblankestijn@gmail.com>
2020-10-01 20:59:39 +0200

    static function with generic generator

--- main.cc
+++ main.cc
@@ -3,10 +3,12 @@
 int main() {
     std::random_device  rand_dev;
 
-    std::set<uint32_t> nums = Unnuun::rand_n_unique(4, 0, 100, std::mt19937(rand_dev()) );
+    auto nums = Unnuun::rand_n_unique(4, 0, 100, std::mt19937(rand_dev()) );
 
     for(auto i : nums)
         std::cerr << i << " ";
+
+    std::cerr << std::endl;
 
     return 1;
 }
--- unnuun.h
+++ unnuun.h
@@ -2,16 +2,14 @@
     Method to generate a random series of Unique Numbers from a Uniform Distribution
 */
 
-#include <functional>
+#include <type_traits>
 #include <cassert>
 #include <random>
 #include <set>
 #include <iostream>
 
-//TODO generic type
 class Unnuun {
     private:
-        //TODO parametrize rand generator
         std::random_device  rand_dev;
         std::mt19937        generator;
 
@@ -21,13 +19,13 @@ class Unnuun {
         std::set<uint32_t> rand_n_unique(const size_t n, size_t max);
         std::set<uint32_t> rand_n_unique(const size_t n, size_t min, size_t max);
 
-        template<typename T>
-        static std::set<uint32_t> rand_n_unique(const size_t n, size_t min, size_t max,
-            T gen_func)
+        template<typename F>
+        static std::set< typename std::result_of<F()>::type > //return set of whatever `F gen_func` returns
+            rand_n_unique(const size_t n, size_t min, size_t max, F gen_func)
         {
             assert(min < max); assert(n < max - min);
 
-            std::set<uint32_t> gen;
+            std::set< typename std::result_of<F()>::type > gen;
 
             for (size_t i = 0; i < n; i++) {
                 size_t r = min + gen_func()%(max - min);
