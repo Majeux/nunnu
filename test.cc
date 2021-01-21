324ede329949b11d78e53a7a6d417ed0038d7d23
Majeux <majeux@pop-os.localdomain>
2021-01-21 11:43:43 +0100

    prints did not influence time + fixed output format

--- test.cc
+++ test.cc
@@ -19,11 +19,6 @@ auto run_naive(size_t n, size_t max) {
 
     auto t2 = std::chrono::steady_clock::now();
 
-    for(auto i : s) {
-        std::cerr << i << ' ';
-    }
-	std:: cerr << std::endl;
-
     return t2-t1;
 }
 
@@ -32,18 +27,12 @@ auto run_vec_alloc(size_t n, size_t max) {
 
     std::vector<uint_fast32_t> v;
     v.reserve(n);
-    for(size_t i = 0; i < max; i++) v.push_back(i);
-    std::shuffle(v.begin(), v.end(), generator);
 
+    for(size_t i = 0; i < max; i++) 
+		v.push_back(i);
+    
+	std::shuffle(v.begin(), v.end(), generator);
     auto t2 = std::chrono::steady_clock::now();
-
-    size_t count = 0;
-    for(auto i : v) {
-        std::cerr << i << ' ';
-        if(count++ >= n-1)
-            break;
-    }
-	std:: cerr << std::endl;
 
     return t2-t1;
 }
@@ -51,60 +40,36 @@ auto run_vec_alloc(size_t n, size_t max) {
 auto run_vec_prealloc(size_t n, size_t max) {
     std::vector <uint_fast32_t> v;
     v.reserve(n);
-    for(size_t i = 0; i < max; i++) v.push_back(i);
+
+    for(size_t i = 0; i < max; i++) 
+		v.push_back(i);
 
     auto t1 = std::chrono::steady_clock::now();
-
     std::shuffle(v.begin(), v.end(), generator);
-
     auto t2 = std::chrono::steady_clock::now();
-
-    size_t count = 0;
-    for(auto i : v) {
-        std::cerr << i << ' ';
-        if(count++ >= n-1)
-            break;
-    }
-	std:: cerr << std::endl;
 
     return t2-t1;
 }
 
 auto run_unnu(size_t n, size_t max) {
     auto t1 = std::chrono::steady_clock::now();
-
     std::set<uint_fast32_t> s = Unnu::n_unique_from(generator, n, max);
-
     auto t2 = std::chrono::steady_clock::now();
-
-    for(auto i : s) {
-        std::cerr << i << ' ';
-	}
-	
-	std:: cerr << std::endl;
 
     return t2-t1;
 }
 
 auto run_unnuOLD(size_t n, size_t max) {
     auto t1 = std::chrono::steady_clock::now();
-
     std::set<uint_fast32_t> s = Unnu::n_unique_fromOLD(generator, n, max);
-
     auto t2 = std::chrono::steady_clock::now();
-
-    for(auto i : s) {
-        std::cerr << i << ' ';
-	}
-	
-	std:: cerr << std::endl;
 
     return t2-t1;
 }
 
 int main() {
     size_t n =   100;
-    size_t max = 100000;
+    size_t max = 100000000;
     /*
         100 out of 100000000
         run_vec_alloc:		 8151040435
@@ -130,8 +95,8 @@ int main() {
     auto t_unnuOLD = run_unnuOLD(n, max);
 
     std::cout << n << " out of " << max << std::endl;
-	std::cout << "run_naive:\t\t " << t_n.count() << std::endl;
 	std::cout << "______________" << std::endl;
+	std::cout << "run_naive:\t\t " << t_n.count() << std::endl;
     std::cout << "run_vec_alloc:\t\t " << t_v.count() << std::endl;
     std::cout << "run_vec_prealloc:\t " << t_vpre.count() << std::endl;
     std::cout << "run_unnu:\t\t " << t_unnu.count() << std::endl;
